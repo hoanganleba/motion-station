a1474f06f933d75b4bead8963987a1d13717fbce
hoanganleba <hoanganleba>
2021-02-23 21:27:34 +0700

    Modify for deployment

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
+# Motion Station
 This project is written in SCSS and Bootstrap
 
 All scss files are compiled into folder css and all html files using the same style.css file
 
 For script.js using jquery for animation of sidebar
 
-To run this file type npm install and gulp watch
+### To run the project 
+
+```
+yarn install
+
+gulp serve
+```
+
+### Compiles and minifies for production
+
+```
+gulp build
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,64 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const runSequence = require("gulp4-run-sequence");
+const browserSync = require("browser-sync").create();
 
-// compile scss to css 
-function style() {
-    // location of scss file
-    return gulp.src('./scss/**/*.scss')
-        // pass it through sass compiler
-        .pipe(sass())
-        // location the compiled css
-        .pipe(gulp.dest('./css'))
-        // stream changes to all browser
-        .pipe(browserSync.stream());
-}
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./**/*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
-}
+// Clean dist folder
+gulp.task("clean:dist", () => {
+  return del("dist/**", { force: true });
+});
 
-exports.style = style;
-exports.watch = watch;
+gulp.task("sass", () => {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("compress:css", () => {
+  return gulp
+  .src("app/css/*.css")
+  .pipe(cleanCSS({ compatibility: "ie8" }))
+  .pipe(gulp.dest("dist/css"));
+})
+
+gulp.task("compress:js", () => {
+  return gulp.src("app/js/**/*.js").pipe(uglify()).pipe(gulp.dest("dist/js"));
+})
+
+gulp.task("compress:html", () => {
+  return gulp
+  .src("app/**/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("dist"));
+})
+
+gulp.task("compress:images", () => {
+  return gulp.src("app/assets/**/*")
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/assets'))
+})
+
+gulp.task("serve", async () => {
+  browserSync.init({
+    server: {
+      baseDir: "app",
+    },
+  });
+  gulp.watch("app/scss/**/*.scss", sass);
+  gulp.watch("app/**/*.html").on("change", browserSync.reload);
+  gulp.watch("app/js/**/*.js").on("change", browserSync.reload);
+});
+
+gulp.task("compress", gulp.series('compress:html', 'compress:css', "compress:js", "compress:images"));
+
+// Series of tasks perform in order
+gulp.task("build", (cb) => {
+  runSequence("sass", "clean:dist", "compress", cb);
+});
